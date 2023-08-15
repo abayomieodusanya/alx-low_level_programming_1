92f4f2c231c4ff0eb5885d362c84c709e66564b3
abayomi odusanya <digitalcoreglobal@gmail.com>
2023-08-14 18:33:12 -0700

    Write a program that prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints all possible combinations of single-digit numbers.
+ *              Numbers are separated by ", ", and printed in ascending order.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + '0'); /* Print the digit */
+		
+		if (num != 9)
+		{
+			putchar(','); /* Print comma */
+			putchar(' '); /* Print space */
+		}
+	}
+
+	putchar('\n'); /* Print a newline at the end */
+
+	return (0);
+}
+
