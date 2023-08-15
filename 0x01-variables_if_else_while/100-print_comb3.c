987e19764981c0dbba590ab2f0bc127cb79f154a
abayomi odusanya <digitalcoreglobal@gmail.com>
2023-08-14 18:41:49 -0700

    Write a program that prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,25 +14,25 @@
  */
 int main(void)
 {
-    int num1, num2;
+	int num1, num2;
 
-    for (num1 = 0; num1 < 10; num1++)
-    {
-        for (num2 = num1 + 1; num2 < 10; num2++)
-        {
-            putchar(num1 + '0'); /* Print the first digit */
-            putchar(num2 + '0'); /* Print the second digit */
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = num1 + 1; num2 < 10; num2++)
+		{
+			putchar(num1 + '0'); /* Print the first digit */
+			putchar(num2 + '0'); /* Print the second digit */
 
-            if (num1 != 8 || num2 != 9)
-            {
-                putchar(','); /* Print comma */
-                putchar(' '); /* Print space */
-            }
-        }
-    }
+			if (num1 != 8 || num2 != 9)
+			{
+				putchar(','); /* Print comma */
+				putchar(' '); /* Print space */
+			}
+		}
+	}
 
-    putchar('\n'); /* Print a newline at the end */
+	putchar('\n'); /* Print a newline at the end */
 
-    return (0);
+	return (0);
 }
 
