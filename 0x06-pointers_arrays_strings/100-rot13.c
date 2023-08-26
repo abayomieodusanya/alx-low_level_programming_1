f30d158bc6c4d60ba70a5ff4abdb105169ecbfc3
abayomi odusanya <digitalcoreglobal@gmail.com>
2023-08-25 22:08:39 -0700

    task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - Encodes a string using ROT13
+ * @s: Input string
+ *
+ * Return: Pointer to the encoded string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char *x = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char *y = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; x[j] != '\0'; j++)
+		{
+			if (s[i] == x[j])
+			{
+				s[i] = y[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
