2c534a2e0b2a67453377cb6c67bc8e6871e53c54
abayomi odusanya <digitalcoreglobal@gmail.com>
2023-08-25 21:43:37 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compare string values
+ * @s1: First input string
+ * @s2: Second input string
+ *
+ * Return: The difference between the ASCII values of the first differing characters
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+
+	return (0);
+}
