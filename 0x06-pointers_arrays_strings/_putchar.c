9f5377206e0a81f3a460de8791db072e2114fed5
abayomi odusanya <digitalcoreglobal@gmail.com>
2023-08-25 20:44:46 -0700

    Write a function that concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+* _putchar – writes the character c to stdout
+* @c: char to be printed
+* Return: 0 on success
+Int _putchar(char c)
+{
+	Return (write(1, &c, 1));
+
+}
